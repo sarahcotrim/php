170f570e4c431cf02547f977af1d67b02b5afb52
sarahcotrim <145464203+sarahcotrim@users.noreply.github.com>
2023-11-08 07:48:33 -0300

    Add files via upload

---
+++ aula02/exp01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 style= " background-color: lightpink"> Damon Torrance </h1>
+    <h2 style= " background-color: lightblue" >Minha mulher não tem que pedir permissão. 
+    Ela é uma força da natureza. </h2>
+    <h3 style= " background-color: lightgoldenrodyellow"> eu sabia que era fodido. 
+    So nao me importava. </h3>
+    <h4 style= " background-color: lightcoral"> Estamos todos apenas fingindo que sabemos o que estamos fazendo. </h4>
+</body>
+</html>
---
+++ aula02/exp02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp02.css">
+</head>
+<body>
+    <h1>Devil's Night</h1>
+    <p>Noite do Diabo</p>
+    <h2>La Nuit du Diable</h2>
+    <p>La Noche del Diablo</p>
+    <h3>La Notte del Diavolo</h3>
+    <h4>Diaboli nox</h4>
+    <h5>Diabla Nokto</h5>
+</body>
+</html>
